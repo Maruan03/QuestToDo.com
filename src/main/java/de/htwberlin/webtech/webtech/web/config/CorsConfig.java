183e8736fd47d6f751a370aa58166aab08fe05ef
BalloutZeinab <zeinab.ballout@student.htw-berlin.de>
2024-01-10 20:36:55 +0100

    Backend kann mit Frontend kommunizieren

--- src/main/java/de/htwberlin/webtech/webtech/web/config/CorsConfig.java
+++ src/main/java/de/htwberlin/webtech/webtech/web/config/CorsConfig.java
@@ -1,24 +1,22 @@
 package de.htwberlin.webtech.webtech.web.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Arrays;
-
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/*")
-                .allowedOriginPatterns("http://localhost:5173") // Frontend-URL eingefügt
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowCredentials(true);
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedMethod("*"); // Erlaubt alle HTTP-Methoden (GET, POST, PUT, DELETE, etc.)
+        config.addAllowedHeader("*"); // Erlaubt alle Header
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
